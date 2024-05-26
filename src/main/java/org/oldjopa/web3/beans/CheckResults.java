6f45d17a6ae4f5864fb21b58cb9a658a9629b258
Leonid <50008736+lolkek22854@users.noreply.github.com>
2024-05-26 22:04:15 +0300

    connected with jconsole

--- src/main/java/org/oldjopa/web3/beans/CheckResults.java
+++ src/main/java/org/oldjopa/web3/beans/CheckResults.java
@@ -1,15 +1,20 @@
 package org.oldjopa.web3.beans;
 
 import jakarta.annotation.ManagedBean;
-import jakarta.enterprise.concurrent.ManagedExecutorDefinition;
+import jakarta.enterprise.context.Initialized;
+import jakarta.enterprise.event.Observes;
 import org.oldjopa.web3.db.DBHandler;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Named;
-import org.oldjopa.web3.jmx.MeanIntervalStatistics;
-import org.oldjopa.web3.jmx.MeanIntervalStatisticsMXBean;
 import org.oldjopa.web3.jmx.PointStatisticsMXBean;
+import org.oldjopa.web3.listners.MBeanRegistry;
 import org.oldjopa.web3.utils.CheckResult;
 import org.oldjopa.web3.utils.Point;
+
+import javax.management.AttributeChangeNotification;
+import javax.management.MBeanNotificationInfo;
+import javax.management.Notification;
+import javax.management.NotificationBroadcasterSupport;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,9 +22,14 @@ import java.util.Collections;
 @Named("results")
 @ManagedBean
 @ApplicationScoped
-public class CheckResults implements Serializable {
+public class CheckResults extends NotificationBroadcasterSupport implements Serializable, PointStatisticsMXBean {
     private ArrayList<CheckResult> results = DBHandler.getResults();
     private Point point = new Point();
+    private long msgCouner = 0;
+
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object unused) {
+        MBeanRegistry.registerBean(this, "checkResults");
+    }
 
     public ArrayList<CheckResult> getResults(){
         ArrayList<CheckResult> reversed = new ArrayList<>(results);
@@ -27,10 +37,24 @@ public class CheckResults implements Serializable {
         return reversed;
     }
 
+    private boolean isInCanvasArea(CheckResult checkResult){
+        return (checkResult.getX() >= -3)&(checkResult.getX() <= 3)&(checkResult.getY() >= -3)&(checkResult.getY() <= 3);
+    }
+
     public void addPoint(){
 //        System.out.println(point);
         CheckResult check = point.check();
         results.add(check);
+        if(!isInCanvasArea(check)) {
+            System.out.println("notification");
+            Notification notification = new Notification(
+                    "Outside of area",
+                    getClass().getSimpleName(),
+                    msgCouner++,
+                    String.format("The point %f %f %f is outside of area.", check.getX(), check.getY(), check.getR())
+            );
+            sendNotification(notification);
+        }
         DBHandler.addResult(check);
     }
 
@@ -46,12 +70,12 @@ public class CheckResults implements Serializable {
         DBHandler.clearResults();
     }
 
-//    @Override
+    @Override
     public long getHitsCounter() {
         return results.stream().filter(CheckResult::getHit).count();
     }
 
-//    @Override
+    @Override
     public long getPointCounter() {
         return results.size();
     }
--- src/main/java/org/oldjopa/web3/jmx/MeanIntervalStatistics.java
+++ src/main/java/org/oldjopa/web3/jmx/MeanIntervalStatistics.java
@@ -1,8 +1,12 @@
 package org.oldjopa.web3.jmx;
 
 import jakarta.annotation.ManagedBean;
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.Initialized;
+import jakarta.enterprise.event.Observes;
 import org.oldjopa.web3.db.DBHandler;
+import org.oldjopa.web3.listners.MBeanRegistry;
 import org.oldjopa.web3.utils.CheckResult;
 
 import java.io.Serializable;
@@ -12,6 +16,10 @@ import java.util.stream.IntStream;
 @ManagedBean
 @ApplicationScoped
 public class MeanIntervalStatistics implements Serializable, MeanIntervalStatisticsMXBean {
+//    @PostConstruct
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object unused){
+        MBeanRegistry.registerBean(this, "checkResults");
+    }
     public long getMeanClickInterval(){
         ArrayList<CheckResult> data = DBHandler.getResults();
         return IntStream.range(1, data.size()).mapToObj(i -> data.get(i).getTimeClick() - data.get(i-1).getTimeClick()).toList().stream().mapToLong(Long::longValue).sum()/(data.size()-1);
